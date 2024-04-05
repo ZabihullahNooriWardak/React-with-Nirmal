36a5b6cf2dcd2c02cb3c8f4ded8497d403345e6a
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-05 13:22:02 +0430

    trying create counter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App(){
+    let [counter,setCounter]=useState(0);
+  
+  return (<div>
+   <p> this is my Counter : {counter}</p>
+   <button onClick={setCounter(counter+=1)}>click</button>
+  </div>)
 }
 
 export default App;
