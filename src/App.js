3fc4f3755c5311c5e5c9ed23edb14ae87ed5efa8
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-09 09:11:08 +0430

    jsx another way

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
-
+let myMessage = React.createElement("h2",{style:{color:'red'}},"hello dear User!")
 function App(){
     let [counter,setCounter]=useState(0);
 function counterFunction(op){
@@ -12,6 +12,7 @@ function counterFunction(op){
   }
 }
   return (<div>
+    {myMessage}
    <p> this is my Counter : {counter}</p>
    <button className="st" onClick={()=>counterFunction('+')}>increment</button>
    <button style={{height:"50px",width:"80px",backgroundColor:"blue"}} onClick={()=>counterFunction('-')}>decrement</button>
