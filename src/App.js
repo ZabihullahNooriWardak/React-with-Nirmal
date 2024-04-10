5899746bf18c700bf27b398e09ef7e3430db1b77
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-10 11:55:40 +0430

    adds class based component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { Component } from 'react';
 import "./App.css"
-function App(){
-    let [obj,setObj]=useState({counter:0,booleanVale:true});
-function counterFunction(op){
-  if(op==='+'){
-    let newCounter=obj.counter+1
-    setObj({...obj,counter:newCounter})
-  }else{
-
-setObj({...obj,counter:obj.counter-1})
-  }
+class App extends Component{
+   render(){
+    return (<div>hello Afghanistan</div>)
+   }
 }
-  return (<div>
-   <p> this is my Counter : {obj.counter}</p>
-   <button className="st" onClick={()=>counterFunction('+')}>increment</button>
-   <button style={{height:"50px",width:"80px",backgroundColor:"blue"}} onClick={()=>counterFunction('-')}>decrement</button>
-   <h1>{obj.booleanVale.toString()}</h1>
-   <button onClick={()=>setObj({...obj,booleanVale:!obj.booleanVale})}>toggle</button>
-  </div>)
-}
-
 export default App;
 
 
