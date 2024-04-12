581195cca0d6b2ce4645c0b737897f2a3eef4458
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-12 21:03:57 +0430

    arrow function and the function syntax are same

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import "./App.css"
 function App(){
      let [date,setDate]=useState(new Date())
-useEffect(function(){
-    setTimeout(() => {
- setDate(new Date());
-    }, 1000);
-});
+useEffect(()=>{
+  setTimeout(() => {
+    setDate(new Date());
+       }, 1000)
+}
+);
   return (<div>
 {date.toLocaleTimeString()}
   </div>)
