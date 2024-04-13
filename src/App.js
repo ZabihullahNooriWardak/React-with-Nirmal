4a887c1f90d969e45e5be8669a8071c4132f8c9f
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-13 18:12:37 +0430

    just like toturial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Currenttiming from './timer';
 import "./App.css"
 function App(){
-  return <Currenttiming />
+ let [showTimer,setShowTimer]=useState(true);
+  return (<div>
+    {showTimer?   <Currenttiming />:<h1>No date and clock</h1>}
+    <button onClick={()=>setShowTimer(!showTimer)}>Hide or show the Timer</button>
+  </div>
+  
+)
 }
 export default App;
+
 
 
 
--- src/timer.jsx
+++ src/timer.jsx
@@ -1,21 +1,20 @@
     import React, { useEffect, useState } from "react";
     function Currenttiming(){
         let [date,setDate]=useState(new Date().toString());
-        let [showTimer,setShowTimer]=useState(true);
+      
         useEffect(()=>{
-            let timer=setInterval(() => {
-                
+            let timer=setInterval(() => {   
                 setDate(new Date().toString());
             }, 1000);
             return ()=>{
                 console.log("clean up code goes here");
                 clearInterval(timer);
             }
-        },[showTimer])
+        },[])
         return (
             <div>
-            <h1>{showTimer ? date: (<p>No Clock and date to display</p>)} </h1>
-            <button onClick={()=>setShowTimer(!showTimer)}>Hide or show the Timer</button>
+            <h1>{date} </h1>
+           
             </div>
         )
     }
