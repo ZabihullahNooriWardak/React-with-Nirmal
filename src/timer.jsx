996f45fc931e7af5d447edb19fba168d1d437d7e
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-13 16:22:38 +0430

    the complete version

--- src/timer.jsx
+++ src/timer.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from "react";
-function Currenttiming(){
-    let [date,setDate]=useState(new Date().toString());
-    useEffect(()=>{
-        let timer=setInterval(() => {
-            
-            setDate(new Date().toString());
-        }, 1000);
-        return ()=>{
-            console.log("clean up code goes here");
-            clearInterval(timer);
-        }
-    },[])
-    return (
-        <h1>{date}</h1>
-    )
-}
-export default Currenttiming;
+    import React, { useEffect, useState } from "react";
+    function Currenttiming(){
+        let [date,setDate]=useState(new Date().toString());
+        let [showTimer,setShowTimer]=useState(true);
+        useEffect(()=>{
+            let timer=setInterval(() => {
+                
+                setDate(new Date().toString());
+            }, 1000);
+            return ()=>{
+                console.log("clean up code goes here");
+                clearInterval(timer);
+            }
+        },[showTimer])
+        return (
+            <div>
+            <h1>{showTimer ? date: (<p>No Clock and date to display</p>)} </h1>
+            <button onClick={()=>setShowTimer(!showTimer)}>Hide or show the Timer</button>
+            </div>
+        )
+    }
+    export default Currenttiming;
