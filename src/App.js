8a56c345bf8ea21380b3cd99a012623535936fe1
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-15 17:25:26 +0430

    amazing function programming technic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
- function App() {
-  let [paragraph,setParagraph]=useState("");
-  let txtBox=(e)=>{
+
+function App() {
+  const [paragraph, setParagraph] = useState("");
+  
+  const txtBox = (additionalParameter) => (e) => {
+    // You can access both the event object (e) and the additional parameter here
+    console.log("Event:", e);
+    console.log("Additional Parameter:", additionalParameter);
     setParagraph(e.target.value);
   }
-  return <React.Fragment>
-    <p>the TextBox data is here : {paragraph}</p>
-     <input onChange={txtBox}></input>
-  </React.Fragment>
+
+  return (
+    <React.Fragment>
+      <p>The TextBox data is here: {paragraph}</p>
+      <input onChange={txtBox("additionalParameter")} />
+    </React.Fragment>
+  );
 }
+
 export default App;
-
-
-
