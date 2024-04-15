bc0ec19c7bd6314537b58302224749fe0d41d265
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-15 19:42:51 +0430

    complete the practice given by teacher

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
 
 function App() {
-  let [paragraph, setParagraph] = useState("");
-
-  let txtBox = (e, additionalParam) => {
-    setParagraph(e.target.value);
-    console.log("Additional parameter:", additionalParam);
-  }
-
+  let [firstName,setFirstName]=useState("")
+  let [lastName,setLastName]=useState("")
+  let [mobile,setMobile]=useState("")
+  let [allData,setAllData]=useState("No data yet")
+function FirstName(e){
+  setFirstName(e.target.value)
+}
+function LastName(e){
+  setLastName(e.target.value)
+}
+function Mobile(e){
+  setMobile(e.target.value)
+}
+function submission(e){
+  e.preventDefault();
+setAllData(firstName+","+lastName+","+mobile);
+}
   return (
     <React.Fragment>
-      <p>The TextBox data is here: {paragraph}</p>
-      <input onChange={(e) => txtBox(e, 123)}></input>
+  <form onSubmit={submission}>
+    <input type="text" placeholder="First name" onChange={FirstName}></input>
+    <br></br>
+    <input type="text" placeholder="last name" onChange={LastName}></input>
+    <br></br>
+    <input type="text" placeholder="Mobile number" onChange={Mobile}></input>
+    <br></br>
+    <input type="submit" value="submit"></input>
+  </form>
+  {allData}
     </React.Fragment>
   );
 }
