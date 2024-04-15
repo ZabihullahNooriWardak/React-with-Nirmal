4ba6f9ccdb79e6de55275230f1d30d45273aeef5
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-15 17:58:26 +0430

    practice functional programming just another way

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 
 function App() {
-  const [paragraph, setParagraph] = useState("");
-  
-  const txtBox = (additionalParameter) => (e) => {
-    // You can access both the event object (e) and the additional parameter here
-    console.log("Event:", e);
-    console.log("Additional Parameter:", additionalParameter);
+  let [paragraph, setParagraph] = useState("");
+
+  let txtBox = (e, additionalParam) => {
     setParagraph(e.target.value);
+    console.log("Additional parameter:", additionalParam);
   }
 
   return (
     <React.Fragment>
       <p>The TextBox data is here: {paragraph}</p>
-      <input onChange={txtBox("additionalParameter")} />
+      <input onChange={(e) => txtBox(e, 123)}></input>
     </React.Fragment>
   );
 }
