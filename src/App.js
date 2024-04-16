fffc7d996c19476571abb088859e38d15f197862
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-16 07:16:06 +0430

    optimized a bit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 function App() {
 const [persInfo,setpersInfo]=useState({name:"",lastname:"",mobile:""})
 function onchangeMethod(e){
-setpersInfo({...persInfo,[e.target.name]:e.target.value})
+  setpersInfo((pState)=>({...pState,[e.target.name]:e.target.value}))
 }
-
+function myFunc(e){
+  e.preventDefault();
+}
   return (
     <React.Fragment>
-  <form>
+  <form onSubmit={myFunc}>
     <input type="text" placeholder="First name" name="name" onChange={onchangeMethod}></input>
     <br></br>
     <input type="text" placeholder="last name" name="lastname" onChange={onchangeMethod}></input>
