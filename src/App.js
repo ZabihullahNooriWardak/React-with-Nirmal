cd2939b67e0517b18e5a13f9aab91a62badc0079
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-17 16:51:11 +0430

    update the whole project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
-import Product from "./components/products"
-function App() {
-  let [isAdmin,setIsAdmin]=useState(true);
- function chkBoxHandler(e){
-    setIsAdmin(e.target.checked)
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
-  
-  return<div>
-  <input type="checkbox" onChange={chkBoxHandler}></input><label>is admin?</label>
-  <hr></hr>
-  <Product isAdmin={isAdmin}/>
-  </div>
-  
 }
+
 export default App;
--- src/components/products.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Product(props){
-    let products=[{id:1,name:"Apple"
-    },{id:2,name:"banana"},{
-        id:3,name:"pear"
-    }]
-    return (props.isAdmin ?products.map((item)=><p>{item.name}</p>) : <p>nothing or no data </p> )
-}
-export default Product;
