a955b13250646575015b5d94c8c8e7cbef976d87
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-21 20:15:23 +0430

    a commit for one hour studying at least

--- src/Course.js
+++ src/Course.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { Link, useMatch } from "react-router-dom";
 function Course(){
+    let usematch=useMatch();
+console.log(usematch);
     return <div>
         <ul>
-            <li><h3>Javascript interveiw question</h3></li>
+            <Link><li><h3>Javascript interveiw question</h3></li></Link>
+            <Link>
             <li><h3>React with interview Questions</h3></li>
+            </Link> 
+            <Link>   
             <li><h3>Algorithm and Data structure</h3></li>
+            </Link>
         </ul>
     </div>
 }
