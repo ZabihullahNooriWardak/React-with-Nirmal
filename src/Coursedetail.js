381d07aa5d4477d7d067ba2ed755e70ae52ddd0f
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-22 15:34:49 +0430

    one commit for today

--- src/Coursedetail.js
+++ src/Coursedetail.js
@@ -3,7 +3,7 @@ import { useParams} from "react-router-dom";
 function Coursedetail(){
    let myParams=useParams();
     return <div>
-<h2>coursedetail page ...{myParams.details}</h2>
+<h2>coursedetail page ..{myParams.details}</h2>
     </div>
 }
 export default Coursedetail;
