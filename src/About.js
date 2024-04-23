d21890a04a8aadcb3fe2bc9e3aa9d49bb189d3ed
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-23 21:48:28 +0430

    fjdks

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 function About(){
-    let style={border:"0.788px solid grey"}
+    let style={border:"0.7388px solid grey"}
     return <h1 style={style}>About page...</h1>
 }
 export default About;
