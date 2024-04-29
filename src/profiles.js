fc026e774252583548d294c5af3d2df57bc45be7
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-29 16:19:35 +0430

    completes the basic app for learning react and react-redux

--- src/profiles.js
+++ src/profiles.js
@@ -1,9 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Profile(){
     let arr=[1,2,3,4,5,6,7,8,9,10]
+   let counterValue= useSelector(state=>state)
     return <>
     {arr.map((item,index)=><Link to={`/profile/${item}`} key={index}>{`profile${item}`}<br></br></Link>)}
     <Outlet/>
+    <h1>{counterValue}</h1>
     </>
 }
 export default Profile;
