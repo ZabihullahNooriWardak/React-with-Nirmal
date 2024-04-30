dfc105bbe1bd48835bb1672cea3eb8317953e330
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-04-30 11:19:10 +0430

    make the file ready for the redux practice

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React from "react";
-import { Link, Routes,Route } from "react-router-dom";
-import About from "./about";
-import Profile from "./profiles";
-import ProfileDetail from "./profileDetail";
-import Home from "./home";
-import SharedLayout from "./shared-layout";
 function App() {
     return (
-    <>
-    <nav>
-        <ul>
-            <li>
-                <Link style={{marginRight:"5px"}} to="/">Home</Link>
-                <Link style={{marginRight:"5px"}} to="/about">About</Link>
-                <Link style={{marginRight:"5px"}} to="/profile">Profile</Link>
-            </li>
-        </ul>
-    </nav>
-<Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route element={<SharedLayout/>}>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/profile" element={<Profile/>}>
-     <Route path=":id" element={<ProfileDetail/>}/>
-     </Route>
-    </Route>
-
-</Routes>
+        <>
+            <h1>Hello World</h1>
+            <p>This is a React app</p>
     </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-   import store from './store';
-import { Provider } from 'react-redux';
+
 const root =createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-<Provider store={store}>
     <App/>
-    </Provider>
-    </BrowserRouter>
 )
