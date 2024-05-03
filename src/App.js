924cbf49fa73c0ddbce69d68f59e5c7ea6700221
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-05-03 07:21:51 +0430

    clean up the app for redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import Purchase from "./purchase";
-import Login from "./login";
-import Cart from "./cart";
-import Total from "./total";
 import React from "react";
 import './App.css';
 function App() {
     return (
       <div className="App">
-        <Login/>
-        <Purchase/>
-        <Cart/>
-        <Total/>
+        <p>simple sample app</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import { combineReducers } from 'redux';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import productReducer from './store/productR';
-import loginReducer from './store/loginSlice';
-const rootReducer=combineReducers({
-pr:productReducer,
-lr:loginReducer
-});
- const store=createStore(rootReducer);
-
 const root =createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
