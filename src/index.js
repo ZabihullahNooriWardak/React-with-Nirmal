5bfda2362bf4b498c8a46598c9b724cd4a4944e5
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-05-03 07:22:48 +0430

    fix bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 const root =createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
         <App/>
-    </Provider>
 )
