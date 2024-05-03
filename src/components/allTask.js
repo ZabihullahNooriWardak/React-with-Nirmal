7667f1ef0305b86ce3afe74b9c9de9cb3b5efa7d
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-05-03 18:32:10 +0430

    complete the update

--- src/components/allTask.js
+++ src/components/allTask.js
@@ -1,22 +1,56 @@
-import { useSelector } from "react-redux";
-import { deletTask } from "../taskSlice/taskSlice";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteTask, updateTask } from "../taskSlice/taskSlice";
+import { useState } from "react";
 import { ReactComponent as MyIcon } from './deleteIcon.svg';
 
+function AllTask() {
+  const tasksArr = useSelector(state => state.taskList.tasks);
+  const dispatch = useDispatch();
+  const [editIndex, setEditIndex] = useState(null);
+  const [editedTask, setEditedTask] = useState("");
 
-function  AllTask() {
-    let taksArr=useSelector(state=>state.taskList.tasks);
-    let dispatch=useDispatch();
+  const handleEdit = (index, task) => {
+    setEditIndex(index);
+    setEditedTask(task);
+  };
+
+  const handleUpdate = (index) => {
+    dispatch(updateTask({ index, updatedTask: editedTask }));
+    setEditIndex(null);
+    setEditedTask("");
+  };
+
   return (
     <div>
       <h1>All Task</h1>
       <ul>
-
-        {taksArr.length===0? <h4>no Task</h4> :taksArr.map((task,index) => {
-          return <li key={index}>{task}--------------<MyIcon className="icon" onClick={()=>{dispatch(deletTask(index))}}/></li>;
-        })}
+        {tasksArr.length === 0 ? <h4>No Task</h4> :
+          tasksArr.map((task, index) => (
+            <li key={index}>
+              {editIndex === index ? (
+                <>
+                  <input
+                    value={editedTask}
+                    onChange={(e) => setEditedTask(e.target.value)}
+                  />
+                  <button onClick={() => handleUpdate(index)}>Save</button>
+                </>
+              ) : (
+                <>
+                  {task}
+                  <MyIcon
+                    className="icon"
+                    onClick={() => dispatch(deleteTask(index))}
+                  />
+                  <button onClick={() => handleEdit(index, task)}>Edit</button>
+                </>
+              )}
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
 }
+
 export default AllTask;
--- src/taskSlice/taskSlice.js
+++ src/taskSlice/taskSlice.js
@@ -1,17 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 let initialState = { tasks: [] };
-let taskSlice=createSlice({
-    name:"taskManager",
-    initialState,
-    reducers:{
-        addTask: (state, action) => {
-          state.tasks=[...state.tasks,action.payload];
-        },
-        deletTask:(state,action)=>{
-            state.tasks=state.tasks.filter((task,index)=>index!==action.payload)
-        }
+
+let taskSlice = createSlice({
+  name: "taskManager",
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.tasks = [...state.tasks, action.payload];
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((task, index) => index !== action.payload);
+    },
+    updateTask: (state, action) => {
+      const { index, updatedTask } = action.payload;
+      state.tasks[index] = updatedTask;
     }
+  }
 });
-export const {addTask,deletTask}=taskSlice.actions;
+
+export const { addTask, deleteTask, updateTask } = taskSlice.actions;
 export default taskSlice.reducer;
