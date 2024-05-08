33c2c80cd4228117654193071b30ed20885b8e8d
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-05-08 08:52:34 +0430

    adds async await syntax instead of pormises

--- src/components/user.js
+++ src/components/user.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 async function userHandler(){
 let data=await fetch("https://jsonplaceholder.typicode.com/users");
 console.log(data)
+
 let userData=await data.json();
 console.log(userData);
 setUsers(userData);
